b07eefe019a392cb2fe2a448ed618b8c8caf8b8e
ma1088 <ma1088@live.com>
2020-03-29 22:21:49 -0300

    esp8266 começa a inicializar!!
    mas ainda responde doidinho :)

--- alo_som/alo_som.ino
+++ alo_som/alo_som.ino
@@ -1,7 +1,14 @@
 //esp8266
-#define DEBUG true
+#define esp8266gpio0 6
+#define esp8266gpio2 7
+#define esp8266chpd 8
+#define esp8266tx 9
+#define esp8266rx 10
+#define esp8266rst 11
+#define rede "Marilia"
+#define senha "guilherme!"
 #include <SoftwareSerial.h>
-SoftwareSerial esp8266(2, 3);
+SoftwareSerial esp8266(esp8266tx, esp8266rx);
 
 //som
 #define sensorSom 5
@@ -17,14 +24,27 @@ void setup() {
   pinMode(sensorSom,INPUT);
 
   //esp8266
-  esp8266.begin(115200);
-  sendData("AT+RSTrn", 2000, DEBUG);
+  Serial.println("Inicialização ESP-8266");
+  digitalWrite(esp8266gpio0,HIGH);
+  digitalWrite(esp8266gpio2,HIGH);
+  digitalWrite(esp8266chpd,HIGH);
+  digitalWrite(esp8266rst,LOW);
   delay(1000);
-  Serial.println("Versao de firmware");
-  delay(3000);
-  sendData("AT+GMRrn", 2000, DEBUG);
-  sendData("AT+CIOBAUD=19200rn", 2000, DEBUG);
-  Serial.println("** Final **");
+  digitalWrite(esp8266rst,HIGH);
+  delay(1500);
+  esp8266.begin(115200);
+  esp8266.setTimeout(5000);
+  while (!esp8266resposta()) {
+    Serial.println("Reset");
+    esp8266.println("AT+RST");
+  }
+  Serial.println("Teste");
+  esp8266.println("AT");
+  if (esp8266resposta()){
+    esp8266.println("AT+GMR");
+    boolean b = esp8266resposta();
+    b = esp8266resposta();
+  }
 }
 
 void loop() {
@@ -40,25 +60,40 @@ void loop() {
   delay(delayfinal);
 }
 
-//métodos esp8266
-String sendData(String command, const int timeout, boolean debug)
-{
-  // Envio dos comandos AT para o modulo
-  String response = "";
-  esp8266.print(command);
-  long int time = millis();
-  while ( (time + timeout) > millis())
-  {
-    while (esp8266.available())
-    {
-      // The esp has data so display its output to the serial window
-      char c = esp8266.read(); // read the next character.
-      response += c;
+boolean esp8266resposta(){
+  Serial.println("Resposta: ");
+  int limite = 5500;
+  unsigned long chegada = millis();
+  boolean continuar = true;
+  boolean resposta = false;
+  String S = "";
+  unsigned long ultimoChar = 0;
+  while (continuar) {
+    if (esp8266.available()) {
+      char c = esp8266.read();
+      ultimoChar = millis();
+      Serial.print (c);
+      S = S + c;
+      if (c == 10) { //LF
+        byte p = S.indexOf(13);
+        String S1 = S.substring(0,p);
+        if ((S1 == "OK") or (S1 == "ready")) {
+          continuar = false;
+          resposta = true;
+        }
+        if ((S1 == "no change") or (S1 == "ERROR")) {
+          continuar = false;
+          resposta = false;
+        }
+        S="";
+      }
+    }else {
+      if (millis() - chegada > limite) {
+        continuar = false;
+        resposta = false;
+      }
     }
   }
-  if (debug)
-  {
-    Serial.print(response);
-  }
-  return response;
+  Serial.println(".");
+  return resposta;
 }
